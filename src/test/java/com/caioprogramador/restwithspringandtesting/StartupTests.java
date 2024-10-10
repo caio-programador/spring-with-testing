f9f03be1caa12f6e84d29eabdfad10cd3d25ed49
Caio Flôres <caio.florees2005@gmail.com>
2024-10-10 16:35:58 -0300

    First commit

---
+++ src/test/java/com/caioprogramador/restwithspringandtesting/StartupTests.java
@@ -0,0 +1,13 @@
+package com.caioprogramador.restwithspringandtesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
